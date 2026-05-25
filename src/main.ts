d1a605ad8dcaa6748bc4eca0fc366af527f9493d
Albatros <bennismehdi.officiel@gmail.com>
2026-05-25 16:37:01 +0200

    feat: enable CORS for naturalisation.mediarium.net

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['https://naturalisation.mediarium.net'],
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
